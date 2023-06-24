1b7592915c8cef3306072f198d81b54beeb69a71
giftojcs <ojaighog@gmail.com>
2023-06-23 22:21:58 -0600

    Adding a new route to express-server.js file

--- express_server.js
+++ express_server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -24,4 +23,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
 
+app.set("view engine", "ejs");
