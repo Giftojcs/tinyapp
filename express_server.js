fc12fe510643b360c7b6ce32e2f2c61544b93429
giftojcs <ojaighog@gmail.com>
2023-06-26 18:05:58 -0600

    Updated express_server.js

--- express_server.js
+++ express_server.js
@@ -13,68 +13,78 @@ const urlDatabase = {
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+// Root route
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// URLs index route
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
+    username: req.cookies["username"], // Passes the "username" cookie to the template
+    urls: urlDatabase // Passes the urlDatabase object to the template
   };
-  res.render("urls_index", templateVars);
+
+  if (!req.cookies["username"]) {
+    res.cookie("username", "your-username-here"); // Sets a default value for "username" cookie if it doesn't exist
+  }
+
+  res.render("urls_index", templateVars); // Renders the "urls_index" template with the provided variables
 });
 
+// New URL route
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"] // Passes the "username" cookie to the template
+  };
+  res.render("urls_new", templateVars); // Renders the "urls_new" template with the provided variables
 });
 
+// Show URL route
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    username: req.cookies["username"], // Passes the "username" cookie to the template
+    id: req.params.id, // Captures the dynamic parameter ":id"
+    longURL: urlDatabase[req.params.id] // Retrieves the corresponding longURL from the urlDatabase
   };
-  res.render("urls_show", templateVars);
+  res.render("urls_show", templateVars); // Renders the "urls_show" template with the provided variables
 });
 
+app.get("/login", (req, res) => {
+  res.render("login"); // Renders the "login" template
+});
+
+app.post("/login", (req, res) => {
+  const { username } = req.body; // Retrieve the username from the request body
+  res.cookie("username", username); // Set the "username" cookie with the provided value
+  res.redirect("/urls"); // Redirect to the URLs index route
+});
+
+// Update URL route
 app.post("/urls/:id", (req, res) => {
   const urlId = req.params.id;
   const newLongURL = req.body.longURL;
   if (urlDatabase[urlId]) {
     urlDatabase[urlId] = newLongURL;
-    res.redirect("/urls");
+    res.redirect("/urls"); // Redirects to the URLs index route after updating the longURL
   } else {
-    res.status(404).send("URL not found");
+    res.status(404).send("URL not found"); // Sends a 404 response if the URL doesn't exist
   }
-});
-
-app.post("/urls/:id/delete", (req, res) => {
-  const urlId = req.params.id;
-  if (urlDatabase[urlId]) {
-    delete urlDatabase[urlId];
-    res.redirect("/urls");
-  } else {
-    res.status(404).send("URL not found");
-  }
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
-  res.redirect("/urls");
+  const username = req.body.username; // Retrieve the username from the request body
+  res.cookie("username", username); // Set the "username" cookie with the provided value
+  res.redirect("/urls"); // Redirect to the URLs index route
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
+
 });
+app.post("/logout", (req, res) => {
+  res.clearCookie("username"); // Clears the "username" cookie
+  res.redirect("/urls"); // Redirects to the URLs index route
+});
+
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
 
